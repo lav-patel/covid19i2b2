83f181cc3a9f77bccaaf4919749194b01cb3f9f4
Lav Patel <contact.lavpatel@gmail.com>
2020-10-04 16:11:07 -0500

    fixxing error

--- export/1.1/covid_daily_counts.sql
+++ export/1.1/covid_daily_counts.sql
@@ -6,5 +6,6 @@ set feedback off
 set markup csv on
 spool covid_daily_counts.csv
 set colsep ','
-select SITEID || ',' || CALENDAR_DATE || ',' || CUMULATIVE_PATIENTS_ALL || ',' || CUMULATIVE_PATIENTS_SEVERE || ',' || CUMULATIVE_PATIENTS_DEAD || ',' || NUM_PAT_IN_HOSP_ON_DATE || ',' || NUM_PAT_IN_HOSPSEVERE_ON_DATE from COVID_DAILY_COUNTS;spool off;
+select SITEID || ',' || CALENDAR_DATE || ',' || CUMULATIVE_PATIENTS_ALL || ',' || CUMULATIVE_PATIENTS_SEVERE || ',' || CUMULATIVE_PATIENTS_DEAD || ',' || NUM_PAT_IN_HOSP_ON_DATE || ',' || NUM_PAT_IN_HOSPSEVERE_ON_DATE from COVID_DAILY_COUNTS;
+spool off;
 exit;
