60157cdd40dbf073298408e868f910011c68f161
Lav Patel <contact.lavpatel@gmail.com>
2021-04-25 21:57:19 -0500

    added -KUMC suffix

--- export/1.1/covid_clinical_course.sql
+++ export/1.1/covid_clinical_course.sql
@@ -4,7 +4,7 @@ set pagesize 0  -- No header rows
 set trimspool on -- remove trailing blanks
 set feedback off
 set markup csv on
-spool ClinicalCourse.csv
+spool ClinicalCourse-KUMC.csv
 set colsep ','
 select SITEID || ',' || DAYS_SINCE_ADMISSION || ',' || NUM_PAT_ALL_CUR_IN_HOSP || ',' || NUM_PAT_EVER_SEVERE_CUR_HOSP from COVID_CLINICAL_COURSE;
 spool off;
--- export/1.1/covid_daily_counts.sql
+++ export/1.1/covid_daily_counts.sql
@@ -4,7 +4,7 @@ set pagesize 0  -- No header rows
 set trimspool on -- remove trailing blanks
 set feedback off
 set markup csv on
-spool DailyCounts.csv
+spool DailyCounts-KUMC.csv
 set colsep ','
 select SITEID || ',' || CALENDAR_DATE || ',' || CUMULATIVE_PATIENTS_ALL || ',' || CUMULATIVE_PATIENTS_SEVERE || ',' || CUMULATIVE_PATIENTS_DEAD || ',' || NUM_PAT_IN_HOSP_ON_DATE || ',' || NUM_PAT_IN_HOSPSEVERE_ON_DATE from COVID_DAILY_COUNTS;
 spool off;
